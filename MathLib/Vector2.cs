f517a618ceda31f56600f7aad8357e696f3bb099
Rahul <rahul.jhugroo99@gmail.com>
2021-03-09 10:04:35 +1100

    Move to Git
    yeah

---
+++ MathLib/Vector2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathLib
+{
+	public struct Vector2
+	{
+		public float x;
+		public float y;
+
+		public Vector2 (float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public Vector2 operator +(Vector2 a, Vector2 b)
+		{
+			return new Vector2()
+		}
+	}
+}
