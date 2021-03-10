5b11de8b478fdba0983b03b2635de563fbe53f3b
Rahul <rahul.jhugroo99@gmail.com>
2021-03-10 16:16:30 +1100

    Added Colour Struct
    Uses RGBA, supports setting and getting of colours from a Uint

--- MathLib/Colour.cs
+++ MathLib/Colour.cs
@@ -4,7 +4,55 @@ using System.Text;
 
 namespace MathLib
 {
-	class Colour
+	struct Colour
 	{
+		public uint Value { get; private set; }
+
+		public Colour(byte red, byte green, byte blue, byte alpha)
+		{
+			Value = (uint)((red << 24) | (green << 16) | (blue << 8) | alpha);
+		}
+
+		public byte GetRed()
+		{
+			return (byte)(Value >> 24);
+		}
+
+		public void SetRed(byte red)
+		{
+			Value = (Value & 0x00FFFFFF) | (uint)(red << 24);
+		}
+
+		public byte GetGreen()
+		{
+			return (byte)(Value >> 16);
+		}
+
+		public void SetGreen(byte green)
+		{
+			Value = (Value & 0xFF00FFFF) | (uint)(green << 16);
+		}
+
+		public byte GetBlue()
+		{
+			return (byte)(Value >> 8);
+		}
+
+		public void SetBlue(byte blue)
+		{
+			Value = (Value & 0xFFFF00FF) | (uint)(blue << 8);
+		}
+
+		public byte GetAlpha()
+		{
+			return (byte)Value;
+		}
+
+		public void SetAlpha(byte alpha)
+		{
+			Value = (Value & 0xFFFFFF00) | alpha;
+		}
+
+
 	}
 }
