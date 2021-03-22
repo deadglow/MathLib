cd37e4c80e7aaa5fe51b006e2ed3d1724f6648c6
Rahul <rahul.jhugroo99@gmail.com>
2021-03-22 16:51:21 +1100

    Added Mathf class
    added deg2rad

---
+++ MathLib/Mathf.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLib
+{
+	public static class Mathf
+	{
+		public const float Deg2Rad = (float)(Math.PI / 180.0);
+		public const float Rad2Deg = (float)(180.0 / Math.PI);
+	}
+}
